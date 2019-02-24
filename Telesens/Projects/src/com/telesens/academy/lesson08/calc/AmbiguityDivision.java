d54e949f42d901c34720a8a9bdf1d60994e24f57
kste4kate <katerina.zhernovaya@gmail.com>
2019-02-24 20:10:05 +0200

    lessons 08. Calculator. Try to use exceptions

---
+++ Telesens/Projects/src/com/telesens/academy/lesson08/calc/AmbiguityDivision.java
@@ -0,0 +1,13 @@
+package com.telesens.academy.lesson08.calc;
+
+public class AmbiguityDivision extends DivisionByZero {
+
+    public AmbiguityDivision(String msg) {
+        super(msg);
+    }
+
+    @Override
+    public String toString() {
+        return "Неопределена опереция для 0/0";
+    }
+}
